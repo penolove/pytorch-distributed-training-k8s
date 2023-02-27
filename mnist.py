ffd32486959781d3cbc346402ae585e37d8e7e48
ChingHua <chinghua.yang@appier.com>
2023-02-27 20:06:37 +0800

    update example

--- mnist.py
+++ mnist.py
@@ -1,117 +1,47 @@
 import os
-
 import torch
-from pytorch_lightning import LightningModule, Trainer
 from torch import nn
-from torch.nn import functional as F
-from torch.utils.data import DataLoader, random_split
-from torchmetrics import Accuracy
-from torchvision import transforms
+import torch.nn.functional as F
 from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+import pytorch_lightning as pl
 
-PATH_DATASETS = os.environ.get("PATH_DATASETS", ".")
-BATCH_SIZE = 32
 
+class LitAutoEncoder(pl.LightningModule):
 
-class LitMNIST(LightningModule):
-    def __init__(self, data_dir=PATH_DATASETS, hidden_size=64, learning_rate=2e-4):
-
+    def __init__(self):
         super().__init__()
-
-        # Set our init args as class attributes
-        self.data_dir = data_dir
-        self.hidden_size = hidden_size
-        self.learning_rate = learning_rate
-
-        # Hardcode some dataset specific attributes
-        self.num_classes = 10
-        self.dims = (1, 28, 28)
-        channels, width, height = self.dims
-        self.transform = transforms.Compose(
-            [
-                transforms.ToTensor(),
-                transforms.Normalize((0.1307,), (0.3081,)),
-            ]
-        )
-
-        # Define PyTorch model
-        self.model = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(channels * width * height, hidden_size),
-            nn.ReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(hidden_size, hidden_size),
-            nn.ReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(hidden_size, self.num_classes),
-        )
-
-        self.accuracy = Accuracy()
-
+        self.encoder = nn.Sequential(nn.Linear(28 * 28, 128), nn.ReLU(), nn.Linear(128, 3))
+        self.decoder = nn.Sequential(nn.Linear(3, 128), nn.ReLU(), nn.Linear(128, 28 * 28))
+    
     def forward(self, x):
-        x = self.model(x)
-        return F.log_softmax(x, dim=1)
+        # in lightning, forward defines the prediction/inference actions
+        embedding = self.encoder(x)
+        return embedding
 
     def training_step(self, batch, batch_idx):
+        # training_step defined the train loop. It is independent of forward
         x, y = batch
-        logits = self(x)
-        loss = F.nll_loss(logits, y)
+        x = x.view(x.size(0), -1)
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        loss = F.mse_loss(x_hat, x)
         return loss
-
-    def validation_step(self, batch, batch_idx):
-        x, y = batch
-        logits = self(x)
-        loss = F.nll_loss(logits, y)
-        preds = torch.argmax(logits, dim=1)
-        self.accuracy(preds, y)
-
-        # Calling self.log will surface up scalars for you in TensorBoard
-        self.log("val_loss", loss, prog_bar=True)
-        self.log("val_acc", self.accuracy, prog_bar=True)
-        return loss
-
-    def test_step(self, batch, batch_idx):
-        # Here we just reuse the validation_step for testing
-        return self.validation_step(batch, batch_idx)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
         return optimizer
-
-    ####################
-    # DATA RELATED HOOKS
-    ####################
-
-    def prepare_data(self):
-        # download
-        MNIST(self.data_dir, train=True, download=True)
-        MNIST(self.data_dir, train=False, download=True)
-
-    def setup(self, stage=None):
-
-        # Assign train/val datasets for use in dataloaders
-        if stage == "fit" or stage is None:
-            mnist_full = MNIST(self.data_dir, train=True, transform=self.transform)
-            self.mnist_train, self.mnist_val = random_split(mnist_full, [55000, 5000])
-
-        # Assign test dataset for use in dataloader(s)
-        if stage == "test" or stage is None:
-            self.mnist_test = MNIST(self.data_dir, train=False, transform=self.transform)
-
-    def train_dataloader(self):
-        return DataLoader(self.mnist_train, batch_size=BATCH_SIZE)
-
-    def val_dataloader(self):
-        return DataLoader(self.mnist_val, batch_size=BATCH_SIZE)
-
-    def test_dataloader(self):
-        return DataLoader(self.mnist_test, batch_size=BATCH_SIZE)
 
 
 def train():
-    model = LitMNIST()
-    trainer = Trainer(accelerator="gpu", devices=2, strategy="ddp", num_nodes=1)
-    trainer.fit(model)
+    dataset = MNIST(os.getcwd(), download=True, transform=transforms.ToTensor())
+    train, val = random_split(dataset, [55000, 5000])
+
+    autoencoder = LitAutoEncoder()
+    trainer = pl.Trainer(max_epochs=1, gpus=2)
+    trainer.fit(autoencoder, DataLoader(train), DataLoader(val))
+    
 
 
 if __name__ == '__main__':
